24411dff809b79b3fcb860eeee6c5698c63cf87f
Caio Cesar <caiopereira1998@hotmail.com>
2021-09-06 16:54:24 -0300

    Basic Static Components

---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Controle de Vendas</a>
+            </div>
+        </nav>
+
+    );
+}
+
+export default NavBar;
