503b89906451f8bdcf2e0d050bffc457ecf362a3
Caio Cesar <caiopereira1998@hotmail.com>
2021-09-08 16:22:46 -0300

    Pagination

---
+++ backend/src/main/java/com/devans/vendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devans.vendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devans.vendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/devans/vendas/service/SaleService.java
@@ -0,0 +1,30 @@
+package com.devans.vendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devans.vendas.dto.SaleDTO;
+import com.devans.vendas.entities.Sale;
+import com.devans.vendas.repositories.SaleRepository;
+import com.devans.vendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+	
+}
